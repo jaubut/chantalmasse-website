54fc5812cf21aec4159dffb5ec0af98c692c8613
jaubut <jeremie.aubut@gmail.com>
2026-03-23 13:00:16 -0400

    trying vercel static.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,7 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   nitro: {
-    preset: 'vercel',
+    preset: 'vercel-static',
   },
 
   modules: [
